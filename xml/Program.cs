a8ccb8430442c83e9b0cec3829147354e7cffa51
djarosz-hub <jaroszdamian9@gmail.com>
2021-02-04 22:54:00 +0100

    initial

---
+++ xml/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using GraZaDuzoZaMalo.Model;
+
+
+namespace AppGraZaDuzoZaMaloCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new KontrolerCLI()).Uruchom();
+
+        }
+
+    }
+}
